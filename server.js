d4eec80646d4e95cfdf4e2ed493a21f0cf95a828
izaanjahangir <izaanjahangir2@gmail.com>
2019-09-01 21:10:31 +0500

    [server] added initual server code

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
