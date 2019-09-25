153a41ab6eaaad1f94bb2c21082b45517e776748
izaanjahangir <izaanjahangir2@gmail.com>
2019-09-25 18:37:24 +0500

    added configuration to host React app on server

--- server.js
+++ server.js
@@ -17,4 +17,14 @@ app.use(express.json());
 // routes
 app.use("/user", require("./route/user"));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("public/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`server is running on port ${port}`));
