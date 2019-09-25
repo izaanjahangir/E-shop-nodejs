324850f7976ebbdc00104157004584517aad68c3
izaanjahangir <izaanjahangir2@gmail.com>
2019-09-25 14:36:25 +0500

    [client] added basic redux configuration

--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./redux/store";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ public/src/redux/auth/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  user: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
