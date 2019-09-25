356a53f9df584788bba647da9d385ca27dfe2277
izaanjahangir <izaanjahangir2@gmail.com>
2019-09-25 20:35:24 +0500

    added home route

--- public/src/config/router.js
+++ public/src/config/router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Home from "../screens/Home";
 import Login from "../screens/Login";
 import Register from "../screens/Register";
 
 export default () => (
   <BrowserRouter>
+    <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
     <Route path="/register" exact component={Register} />
   </BrowserRouter>
