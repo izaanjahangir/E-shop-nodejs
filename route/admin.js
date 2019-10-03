b42723df97ead5f6d3636cc25c0e1e675b8201d5
izaanjahangir <izaanjahangir2@gmail.com>
2019-10-03 21:32:14 +0500

    [general] pulled code

--- route/admin.js
+++ route/admin.js
@@ -4,9 +4,6 @@ const router = express.Router();
 // Controllers
 const adminController = require("../controllers/admin");
 
-
-
 router.post("/login", adminController.login);
-
 
 module.exports = router;
